feefa714788733cb8556ca142839855db7df80d8
Salil Bagga <salilbagga2@gmail.com>
2022-06-07 21:51:03 +0530

    fix: handle rerender of codeEditor

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ import OutputWindow from './components/OutputWindow'
 import LanguageSelector from './components/LanguageSelector'
 import { useRecoilState } from 'recoil'
 import { languageState } from './appContext'
+import { Controller, useForm } from 'react-hook-form'
 
 const App: React.FC = () => {
+  const { control, register, handleSubmit } = useForm()
+
   const baseUrl = import.meta.env.VITE_BASEURL
-  const [code, setCode] = useState('')
+
   const [result, setResult] = useState('')
   const [language, setLanguage] = useRecoilState(languageState)
   console.log(language)
@@ -41,60 +44,66 @@ const App: React.FC = () => {
       console.log('err', err)
     }
   }
-  async function executeCode() {
-    const formData = {
-      language_id: language.id,
-      // encode source code in base64
-      source_code: btoa(code)
+
+  const onSubmit = async (data: any) => {
+    console.log(data)
+    if (data.code) {
+      const formData = {
+        language_id: language.id,
+        // encode source code in base64
+        source_code: btoa(data.code)
+      }
+      const options = {
+        method: 'POST',
+        url: import.meta.env.VITE_BASEURL + '/submissions',
+        params: { base64_encoded: 'true', fields: '*' },
+        headers: {
+          'content-type': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        data: formData
+      }
+      const token = await (await axios.request(options)).data.token
+      // async function getResult() {
+      //   const result = await axios.get(
+      //     `${
+      //       import.meta.env.VITE_BASEURL
+      //     }/submissions/${token}?base64_encoded=true&wait=false&fields=stdout,time,memory,stderr,token,compile_output,message,status`
+      //   )
+      //   return result
+      // }
+      // const res = await getResult()
+      checkStatus(token)
+      console.log('token', token)
     }
-    const options = {
-      method: 'POST',
-      url: import.meta.env.VITE_BASEURL + '/submissions',
-      params: { base64_encoded: 'true', fields: '*' },
-      headers: {
-        'content-type': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      data: formData
-    }
-    const token = await (await axios.request(options)).data.token
-    // async function getResult() {
-    //   const result = await axios.get(
-    //     `${
-    //       import.meta.env.VITE_BASEURL
-    //     }/submissions/${token}?base64_encoded=true&wait=false&fields=stdout,time,memory,stderr,token,compile_output,message,status`
-    //   )
-    //   return result
-    // }
-    // const res = await getResult()
-    checkStatus(token)
-    console.log('token', token)
   }
-  console.log('My first push')
+
   return (
     <div className="m-auto w-4/5">
       <div className="flex flex-col justify-center h-screen">
         <LanguageSelector />
-        <Editor
-          height="80vh"
-          defaultLanguage={language.value || 'javascript'}
-          className="mt-4"
-          theme="vs-dark"
-          onChange={(newValue: string | undefined) =>
-            setCode(() => {
-              if (newValue) return newValue
-              else return ''
-            })
-          }
-          defaultValue="// some comment"
-        />
-        <button
-          type="button"
-          className="btn btn-success w-32 m-4"
-          onClick={executeCode}
-        >
-          Execute
-        </button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            control={control}
+            name="code"
+            render={({ field: { onChange, onBlur, value, name, ref } }) => (
+              <Editor
+                height="50vh"
+                defaultLanguage={language.value || 'javascript'}
+                className="mt-4"
+                theme="vs-dark"
+                onChange={(newValue: string | undefined) => {
+                  if (newValue) onChange(newValue)
+                }}
+                defaultValue="// some comment"
+              />
+            )}
+          />
+          <button type="submit" className="btn btn-success w-32 m-4">
+            Execute
+          </button>
+        </form>
+
         <OutputWindow outputDetails={result} />
       </div>
     </div>
