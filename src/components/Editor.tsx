bc47bee6685db37eb7134c3418fb4a7ba7a4620c
Gaganmeet Bahri <gaganmeet@hybrowlabs.com>
2022-06-09 07:50:11 +0530

    feat: added font size select

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -6,20 +6,41 @@ import { executeCode } from '../utils/executeCode'
 
 const CodeEditor = () => {
   const [code, setCode] = useRecoilState(codeState)
+  const [fontSize, setFontSize] = useState('14')
 
   const language = useRecoilValue(languageState)
   const loading = useRecoilValue(loadingState)
+  const options = { fontSize: fontSize + 'px' }
 
   function handleExecuteCode() {
     executeCode(code, language.id)
   }
   return (
     <>
+      <div
+        className="flex flex-col tooltip tooltip-bottom z-10 w-1/3  place-items-start ml-2"
+        data-tip={fontSize}
+      >
+        <label htmlFor="fontSize">Font Size :</label>
+        <input
+          id="fontSize"
+          type="range"
+          min={12}
+          onChange={(e) => {
+            setFontSize(e.target.value)
+          }}
+          max={36}
+          value={fontSize}
+          className="range range-sm"
+        />
+      </div>
       <Editor
         height="80vh"
         defaultLanguage={language.value || 'javascript'}
         className="mt-4"
+        options={options}
         theme="vs-dark"
+        value="// Write your code here"
         onChange={(newValue: string | undefined) =>
           setCode(() => {
             if (newValue) return newValue
