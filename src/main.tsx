8a119dea5f358cdd5fd0abf790c663a373f0e228
Gaganmeet Bahri <gaganmeet@hybrowlabs.com>
2022-06-10 07:18:15 +0530

    feat: upgrade to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import { RecoilRoot } from 'recoil'
 import DebugObserver from './DebugObserver'
 import RecoilNexus from 'recoil-nexus'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <RecoilNexus />
-      <DebugObserver />
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+if (container) {
+  const root = createRoot(container)
+  root.render(
+    <React.StrictMode>
+      <RecoilRoot>
+        <RecoilNexus />
+        <DebugObserver />
+        <App />
+      </RecoilRoot>
+    </React.StrictMode>
+  )
+}
